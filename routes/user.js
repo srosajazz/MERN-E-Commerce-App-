cb96d378b9f165f2723a015210a0769a71673f2e
Sergio Rosa <scarlosjazz@gmail.com>
2020-01-13 19:12:30 -0500

    Controllers

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("Hello World hey");
-});
+const { sayHi } = require("../controllers/user");
+router.get("/", sayHi);
 
 module.exports = router;
