487aa6d2e50caa348be2b4ce0c4ddde8abd94393
Sergio Rosa <scarlosjazz@gmail.com>
2020-01-26 14:55:57 -0500

    Using express validator

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const expressValidor = require("express-validator");
 require("dotenv").config();
 
 //import routes
@@ -31,6 +32,7 @@ mongoose
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(expressValidor());
 
 //route middleware
 app.use("/api", userRoutes);
