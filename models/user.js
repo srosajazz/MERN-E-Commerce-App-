dad305604d3e527fd04d306430c1d47c0f9d861f
Sergio Rosa <scarlosjazz@gmail.com>
2020-01-26 14:10:12 -0500

    Using Postman to signup user

--- models/user.js
+++ models/user.js
@@ -10,29 +10,25 @@ const userSchema = new mongoose.Schema(
             required: true,
             maxlength: 32,
         },
-
         email: {
             type: String,
+            trim: true,
             required: true,
             unique: true,
         },
-
         hashed_password: {
             type: String,
             required: true,
         },
-
         about: {
             type: String,
             trim: true,
         },
-
         salt: String,
         role: {
             type: Number,
             default: 0,
         },
-
         history: {
             type: Array,
             default: [],
@@ -54,6 +50,10 @@ userSchema
     });
 
 userSchema.methods = {
+    authenticate: function(plainText) {
+        return this.encryptPassword(plainText) === this.hashed_password;
+    },
+
     encryptPassword: function(password) {
         if (!password) return "";
         try {
@@ -66,4 +66,5 @@ userSchema.methods = {
         }
     },
 };
+
 module.exports = mongoose.model("User", userSchema);
