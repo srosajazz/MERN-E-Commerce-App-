d261b64178e3d9fa8b9d5e17d5235c8e892e3fcb
Sergio Rosa <scarlosjazz@gmail.com>
2020-01-29 21:02:55 -0500

    User signin using JWT

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,7 @@
 const User = require("../models/user");
+const jwt = require("jsonwebtoken"); // top generate signed token
+const expressJwt = require("express-jwt"); // for authorization check
+
 const { errorHandler } = require("../helpers/dbErrorHandler");
 exports.signup = (req, res) => {
     // console.log("req.body", req.body);
@@ -14,5 +17,33 @@ exports.signup = (req, res) => {
         res.json({
             user,
         });
+    });
+};
+
+exports.signin = (req, res) => {
+    //find the user based on email
+    const { email, password } = req.body;
+    user.findOne({ email }, (err, user) => {
+        if (err || !user) {
+            return res.status(400).json({
+                error: "User with that email does not exist. Please singup",
+            });
+        }
+        // if user is found make sure the email and password match
+
+        // create authenticate method in user model
+        if (!user.authenticate(password)) {
+            return res.status(401).json({
+                error: "Email and password does not match",
+            });
+        }
+        // generate a signed token with user id and secret
+        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+
+        // persist the token 't'in cookie with expire date
+        res.cookie("t", token, { expire: new Date() + 9999 });
+        // return response with user and token to frontend client
+        const { _id, name, email, role } = user;
+        return res.json({ token, user: { _id, email, name, role } });
     });
 };
